bfa35f3c0755e46ca2ec08b411278a1ee72f46ad
Daniel-Digid <daniel@dig.id>
2026-01-15 17:49:18 +0000

    d

---
+++ template-parts/modules/zimmer-suiten-gallery.php
@@ -0,0 +1,3 @@
+<section id="zimmer-suiten-gallery" class="zimmer-suiten-gallery relative overflow-hidden">
+
+</section>
---
+++ template-parts/posts/zimmer-suiten/content.php
@@ -0,0 +1,23 @@
+<section id="zimmer-suiten-content" class="zimmer-suiten-content relative overflow-hidden">
+  <div class="theme-container">
+    <div class="theme-grid">
+      <div class="col-span-2 md:col-span-4 xl:col-span-7">
+        <div class="intro-block">
+          <h2 class="title-secondary text-dark-2"><?php the_field( 'intro_title' ); ?></h2>
+          <p class="text-dark-2 pt-5 pb-7 xl:py-7"><?php the_field( 'intro_text' ); ?></p>
+        </div>
+        <div class="information-block">
+          <h4 class="title-smaller text-dark-2"><?php the_field( 'information_title' ); ?></h4>
+          <p class="text-dark-2 pt-5 pb-7 xl:py-7"><?php the_field( 'information_check_in' ); ?></p>
+          <p class="text-dark-2 pt-5 pb-7 xl:py-7"><?php the_field( 'information_check_out' ); ?></p>
+        </div>
+        <div class="additional-block">
+          <h3 class="title-small text-dark-2"><?php the_field( 'additional_title' ); ?></h3>
+        </div>
+      </div>
+      <div class="col-span-2 md:col-span-6 xl:col-span-4 xl:col-start-9">
+
+      </div>
+    </div>
+  </div>
+</section>
