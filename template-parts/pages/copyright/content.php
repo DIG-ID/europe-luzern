c81fa373627e55c41817f856e5b15f8c848a7a55
bankz91 <brunoduarte.91300@gmail.com>
2026-01-26 17:39:49 +0000

    1

--- template-parts/pages/copyright/content.php
+++ template-parts/pages/copyright/content.php
@@ -9,8 +9,6 @@
 ?>
 <section id="content-copyright-section" class="content-copyright bg-white">
     <div class="theme-container">
-        <div class="theme-grid">
             <?php the_content();?>
-        </div>
     </div>
 </section>
