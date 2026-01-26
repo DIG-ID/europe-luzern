f715fc0abe62ce6208b2f8e9cbb403c6491670a7
bankz91 <brunoduarte.91300@gmail.com>
2026-01-26 17:34:23 +0000

    1

--- template-parts/pages/copyright/content.php
+++ template-parts/pages/copyright/content.php
@@ -7,7 +7,7 @@
  * @since 1.0.0
  */
 ?>
-<section id="content-copyright-section" class="content-copyright bg-cream">
+<section id="content-copyright-section" class="content-copyright bg-white">
     <div class="theme-container">
         <?php the_content();?>
     </div>
